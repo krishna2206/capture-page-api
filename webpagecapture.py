eaecb945cbde08125ad3ec622e3bd5bfaf6e4b17
Fitiavana Krishna <fitiavana.krishna@gmail.com>
2022-09-25 22:47:14 +0300

    feat: emulate mobile device

--- webpagecapture.py
+++ webpagecapture.py
@@ -5,9 +5,13 @@ from playwright.async_api import async_playwright
 async def generate_webpage_screenshot(page_url):
     async with async_playwright() as playwright:
         try:
+            device = playwright.devices["Pixel 4"]
             browser = await playwright.chromium.launch(headless=True)
+            context = await browser.new_context(**device)
+
             page = await browser.new_page()
             await page.goto(page_url)
+
             filename = f"{str(uuid.uuid4())}.png"
             await page.screenshot(path=f"screenshots/{filename}", full_page=True)
         except Exception as error:

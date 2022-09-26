650a8549262ccf12b671918dbe469eafd5f9ddcd
Fitiavana Krishna <fitiavana.krishna@gmail.com>
2022-09-26 10:22:12 +0300

    removed mobile device emulation

--- webpagecapture.py
+++ webpagecapture.py
@@ -9,9 +9,7 @@ from playwright.async_api import async_playwright
 async def generate_webpage_screenshot(page_url):
     async with async_playwright() as playwright:
         try:
-            device = playwright.devices["Pixel 4"]
             browser = await playwright.chromium.launch(headless=True)
-            context = await browser.new_context(**device)
 
             page = await browser.new_page()
             await page.goto(page_url)
